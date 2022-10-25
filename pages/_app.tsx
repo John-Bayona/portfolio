34776f08e9a2beedfef85b0c96e1d6430c365d93
John <z5512345z@gmail.com>
2022-10-25 11:01:14 +0200

    added conatel

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { ReactElement, ReactNode } from 'react'
+import { NextPage } from 'next'
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+    getLayout?: (page: ReactElement) => ReactNode
+}
+type AppPropsWithLayout = AppProps & {
+    Component: NextPageWithLayout
+}
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+    const getLayout = Component.getLayout ?? ((page) => page)
+    return getLayout(<Component {...pageProps} />)
 }
 
 export default MyApp
