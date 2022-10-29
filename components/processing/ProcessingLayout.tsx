07d0eb6be1275ce55cc959e8756ed66dab0949f1
John <z5512345z@gmail.com>
2022-10-29 02:01:00 +0200

    moved processing

--- components/processing/ProcessingLayout.tsx
+++ components/processing/ProcessingLayout.tsx
@@ -2,26 +2,7 @@ import { createContext, PropsWithChildren } from 'react'
 import Link from 'next/link'
 import { useState } from 'react'
 import React from 'react'
-export const colores = [
-    { nombre: 'Blanco', color: 'rgb(255,255,255)' },
-    { nombre: 'amarillo', color: 'rgb(255, 200, 0)' },
-    { nombre: 'amarillo verdoso', color: 'rgb(205, 200, 0)' },
-    { nombre: 'verde claro', color: 'rgb(155, 200, 0)' },
-    { nombre: 'naranja', color: 'rgb(255, 100, 0)' },
-    { nombre: 'naranja claro', color: 'rgb(255, 150, 0)' },
-    { nombre: 'naranja tierra (ocre)', color: 'rgb(225, 170, 0)' },
-    { nombre: 'azul', color: 'rgb(80, 100, 200)' },
-    { nombre: 'azul turquesa', color: 'rgb(90, 190, 200)' },
-    { nombre: 'azul turquesa oscuro', color: 'rgb(20, 130, 150)' },
-    { nombre: 'rojo', color: 'rgb(205, 30, 0)' },
-    { nombre: 'gris oscuro', color: 'rgb(60, 60, 60)' },
-    { nombre: 'gris', color: 'rgb(150, 150, 150)' },
-    { nombre: 'gris claro', color: 'rgb(180, 180, 180)' },
-]
-export const ColorContext = createContext({
-    nombre: 'black',
-    color: 'rgb(255,255,255)',
-})
+
 function ProcessingLayout({ children }: PropsWithChildren) {
     const [colorActual, seleccionarColorActual] = useState({
         nombre: 'black',
